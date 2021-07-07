2f89fee1041c88d3d236cf6471f5abb22775046e
chen tao <hilerchyn@gmail.com>
2021-07-07 19:47:20 +0800

    implements vertext type

---
+++ dag.go
@@ -0,0 +1,6 @@
+package dag
+
+
+type DAG struct {
+	Vertex
+}
---
+++ vertex.go
@@ -0,0 +1,80 @@
+package dag
+
+import (
+	"container/list"
+	"errors"
+)
+
+type Vertex struct {
+	Parents  *list.List
+	Children *list.List
+	Value    interface{}
+	Hash     string
+	Type     string
+}
+
+func NewVertex(vType, hash string, value interface{}) *Vertex {
+	return &Vertex{
+		Hash:     hash,
+		Type:     vType,
+		Value:    value,
+		Parents:  list.New(),
+		Children: list.New(),
+	}
+}
+
+func (v *Vertex) RemoveChild(hash string) {
+	for e := v.Children.Front(); e != nil; e = e.Next() {
+		val, ok := e.Value.(*Vertex)
+		if !ok {
+			panic(errors.New("type error"))
+		}
+
+		if val.Hash == hash {
+			v.Children.Remove(e)
+		}
+
+	}
+}
+
+func (v *Vertex) RemoveParent(hash string) {
+	for e := v.Parents.Front(); e != nil; e = e.Next() {
+		val, ok := e.Value.(*Vertex)
+		if !ok {
+			panic(errors.New("type error"))
+		}
+
+		if val.Hash == hash {
+			v.Parents.Remove(e)
+		}
+
+	}
+}
+
+func (v *Vertex) IsEqual(vC *Vertex) bool {
+	if v.Hash != vC.Hash {
+		return false
+	}
+
+	if v.Parents.Len() != vC.Parents.Len() {
+		return false
+	}
+
+	e := v.Parents.Front()
+	eC := vC.Parents.Front()
+	for e != nil && eC != nil {
+
+		if e.Value.(*Vertex).Hash != eC.Value.(*Vertex).Hash {
+			return false
+		}
+
+		e.Next()
+		eC.Next()
+	}
+
+	if v.Children.Len() != vC.Children.Len() {
+		return false
+	}
+
+	return true
+}
